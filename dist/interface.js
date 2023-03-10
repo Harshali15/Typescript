095b63f9b3065a38c0ea7b0df9e851a5e61abce5
Harshali15 <harshalijain.1504@gmail.com>
2023-03-10 15:33:20 -0800

    Typescript final

--- dist/interface.js
+++ dist/interface.js
@@ -14,8 +14,8 @@ let [user1, user2] = [
     { id: 2, name: "joe2", email: "" },
     { id: 3, name: "joe3", email: "" },
 ];
-console.log(user1);
-console.log(user2);
+console.log(user1); //first element in the array
+console.log(user2); //second element in the array
 let [user3, user4, ...restUsers] = [
     { id: 1, name: "joe", email: "" },
     { id: 2, name: "joe2", email: "" },
@@ -23,6 +23,6 @@ let [user3, user4, ...restUsers] = [
     { id: 3, name: "joe4", email: "" },
     { id: 3, name: "joe5", email: "" },
 ];
-console.log(user3);
-console.log(user4);
-console.log(restUsers);
+console.log(user3); //first element in the array
+console.log(user4); //second element in the array
+console.log(restUsers); //rets of elements in the array
